f1380ab686062905b535ad31b4a4d3dfbbfb3936
clementleclerc <leclerc92@gmail.com>
2025-08-19 14:32:22 +0200

    Initial Commit

---
+++ patpanic/patpanicApp.swift
@@ -0,0 +1,17 @@
+//
+//  patpanicApp.swift
+//  patpanic
+//
+//  Created by clement leclerc on 19/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct patpanicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
