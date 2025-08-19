e367efddf84f12f20722edb649ced28d8474e64d
clementleclerc <leclerc92@gmail.com>
2025-08-19 14:39:44 +0200

    create ButtonMenu

---
+++ patpanic/components/buttons/ButtonMenu.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonMenu.swift
+//  patpanic
+//
+//  Created by clement leclerc on 19/08/2025.
+//
+
+import SwiftUI
+
+struct ButtonMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonMenu()
+}
